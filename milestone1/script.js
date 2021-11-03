7e08434e8714fee288fcf053355113eb138601bb
flashbLack98 <npalakuttige98@icloud.com>
2021-11-03 22:06:27 +0100

    milestone 1 complete

--- milestone1/script.js
+++ milestone1/script.js
@@ -2,8 +2,26 @@ Vue.config.devtools = true;
 
 
 const vueApp = new Vue({
-    el: "id/classe da inserire",
+    el: "#app",
     data: {
-        username: "Ciao da vue"
+        userList: [
+            {
+                name: "Michele",
+                imgUrl: "../img/avatar_1.jpg"
+            },
+            {
+                name: "Fabio",
+                imgUrl: "../img/avatar_2.jpg"
+            },
+            {
+                name: "Samuele",
+                imgUrl: "../img/avatar_3.jpg"
+            },
+            {
+                name: "Luisa",
+                imgUrl: "../img/avatar_4.jpg"
+            }
+
+        ]
     }
 });
